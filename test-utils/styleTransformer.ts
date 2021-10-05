8514761ba3bc8e81c4bd6ab631c1b80ccfe9a3be
hannawh <hannawh@stud.ntnu.no>
2021-10-05 17:23:14 +0200

    Remove workshop code

--- test-utils/styleTransformer.ts
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    process(src: unknown, filename: string) {
-        return `module.exports = ${JSON.stringify(path.basename(filename))};`;
-    }
-};
--- test-utils/testTextUtil.ts
+++
@@ -1,8 +0,0 @@
-export const testInitalMessages = {
-    'test.text': 'test text',
-    'test.text.title': 'test title',
-    'test.text.text': 'test text text',
-    'test.tooltip': 'test tooltip',
-    'test.dummy': 'test dummy',
-    'test.key': 'test key'
-};
