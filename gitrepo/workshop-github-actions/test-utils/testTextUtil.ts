8505d9a49d0a48532e96a2f6defe6faed2a25ab0
hannawh <hannawh@stud.ntnu.no>
2021-10-05 17:25:07 +0200

    Add correct workshop code

---
+++ gitrepo/workshop-github-actions/test-utils/testTextUtil.ts
@@ -0,0 +1,8 @@
+export const testInitalMessages = {
+    'test.text': 'test text',
+    'test.text.title': 'test title',
+    'test.text.text': 'test text text',
+    'test.tooltip': 'test tooltip',
+    'test.dummy': 'test dummy',
+    'test.key': 'test key'
+};
